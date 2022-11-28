404ff1b619e000b04a9bf014ee9472e5a0077b88
cpj <1300161364@qq.com>
2022-11-29 00:22:36 +0800

    sql

--- db.sql
+++ db.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80026
  File Encoding         : 65001
 
- Date: 25/11/2022 15:48:51
+ Date: 29/11/2022 00:21:44
 */
 
 SET NAMES utf8mb4;
@@ -30,15 +30,18 @@ CREATE TABLE `admin`  (
   `updatetime` datetime NULL DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '密码',
   `status` tinyint(1) NULL DEFAULT 1 COMMENT '状态',
+  `flag` tinyint(1) NULL DEFAULT 0,
+  `sno` char(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `username_key`(`username`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 30 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 38 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of admin
 -- ----------------------------
-INSERT INTO `admin` VALUES (30, 'admin', '15880051262', '1300161364', '2022-11-24 22:13:03', NULL, '123456', 1);
-INSERT INTO `admin` VALUES (31, 'chen', '18859063781', NULL, '2022-11-25 01:01:44', '2022-11-25 15:18:23', '0402', 1);
+INSERT INTO `admin` VALUES (30, 'admin', '15880051262', '1300161364', '2022-11-24 22:13:03', NULL, '123456', 1, 0, NULL);
+INSERT INTO `admin` VALUES (31, 'chen', '18859063781', NULL, '2022-11-25 01:01:44', '2022-11-28 22:33:18', '0402', 1, 0, NULL);
+INSERT INTO `admin` VALUES (38, '陈培杰', '15880051262', NULL, '2022-11-28 21:17:43', NULL, '123456', 1, 1, '032002503');
 
 -- ----------------------------
 -- Table structure for course
@@ -56,8 +59,7 @@ CREATE TABLE `course`  (
 -- ----------------------------
 INSERT INTO `course` VALUES ('1', '数据库');
 INSERT INTO `course` VALUES ('2', '操作系统');
-INSERT INTO `course` VALUES ('3', '高等数学');
-INSERT INTO `course` VALUES ('4', '大学物理');
+INSERT INTO `course` VALUES ('4', '大学语文');
 
 -- ----------------------------
 -- Table structure for sc
@@ -66,27 +68,18 @@ DROP TABLE IF EXISTS `sc`;
 CREATE TABLE `sc`  (
   `sno` char(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `cno` char(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `grade` int NULL DEFAULT 0,
-  PRIMARY KEY (`sno`, `cno`) USING BTREE,
+  `grade` int NULL DEFAULT NULL,
+  `tno` char(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`sno`, `cno`, `tno`) USING BTREE,
   UNIQUE INDEX `SCno`(`sno`, `cno`) USING BTREE,
-  INDEX `cno`(`cno`) USING BTREE,
-  CONSTRAINT `sc_ibfk_1` FOREIGN KEY (`sno`) REFERENCES `student` (`sno`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `sc_ibfk_2` FOREIGN KEY (`cno`) REFERENCES `course` (`cno`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `cno`(`cno`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sc
 -- ----------------------------
-INSERT INTO `sc` VALUES ('001', '1', 100);
-INSERT INTO `sc` VALUES ('001', '2', 99);
-INSERT INTO `sc` VALUES ('002', '3', 80);
-INSERT INTO `sc` VALUES ('003', '4', 85);
-INSERT INTO `sc` VALUES ('004', '2', 88);
-INSERT INTO `sc` VALUES ('005', '2', 88);
-INSERT INTO `sc` VALUES ('006', '3', 60);
-INSERT INTO `sc` VALUES ('007', '1', 95);
-INSERT INTO `sc` VALUES ('032002503', '1', 100);
-INSERT INTO `sc` VALUES ('999', '1', 100);
+INSERT INTO `sc` VALUES ('032002503', '2', NULL, '11');
+INSERT INTO `sc` VALUES ('032002503', '4', NULL, '44');
 
 -- ----------------------------
 -- Table structure for student
@@ -107,28 +100,20 @@ CREATE TABLE `student`  (
 -- ----------------------------
 -- Records of student
 -- ----------------------------
-INSERT INTO `student` VALUES ('001', '张三', '男', 20, 'CS', NULL, NULL);
-INSERT INTO `student` VALUES ('002', '李四', '女', 23, 'MBA', NULL, NULL);
-INSERT INTO `student` VALUES ('003', '王五', '男', 19, 'EE', NULL, NULL);
-INSERT INTO `student` VALUES ('004', '刘明', '女', 20, 'CS', NULL, NULL);
-INSERT INTO `student` VALUES ('005', '许华', '男', 22, 'ME', NULL, NULL);
-INSERT INTO `student` VALUES ('006', '秋赤', '男', 19, 'MS', NULL, NULL);
-INSERT INTO `student` VALUES ('007', '赵四', '男', 18, 'CS', NULL, NULL);
-INSERT INTO `student` VALUES ('032002503', '陈培杰', '男', 20, 'cs', '12345678910', '福州大学');
-INSERT INTO `student` VALUES ('999', 'test', '男', 10, 'test', NULL, NULL);
+INSERT INTO `student` VALUES ('032002503', '陈培杰', '男', 21, 'cs', '15880051262', '福州大学');
 
 -- ----------------------------
 -- Table structure for teacher
 -- ----------------------------
 DROP TABLE IF EXISTS `teacher`;
 CREATE TABLE `teacher`  (
-  `tno` char(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `tno` char(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `cno` char(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `tname` char(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `phone` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`tno`) USING BTREE,
   UNIQUE INDEX `t1`(`phone`) USING BTREE,
-  INDEX `Cno`(`cno`) USING BTREE,
-  CONSTRAINT `teacher_ibfk_1` FOREIGN KEY (`cno`) REFERENCES `course` (`cno`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `Cno`(`cno`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -160,7 +145,7 @@ delimiter ;
 -- ----------------------------
 DROP TRIGGER IF EXISTS `tri`;
 delimiter ;;
-CREATE TRIGGER `tri` AFTER INSERT ON `student` FOR EACH ROW INSERT INTO sc VALUES(NEW.sno,'1',100)
+CREATE TRIGGER `tri` AFTER INSERT ON `student` FOR EACH ROW INSERT INTO admin(username,`password`,phone,flag,sno) VALUES(NEW.sname,"123456",NEW.phone,1,NEW.sno)
 ;;
 delimiter ;
 
